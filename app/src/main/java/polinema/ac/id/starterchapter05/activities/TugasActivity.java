09ca51946ccc2901a4ca1dd5704f46e51253e356
Hilal06 <rifaulhilal06@gmail.com>
2019-10-01 22:23:23 +0700

    membuat action pada activity tugas

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/TugasActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/TugasActivity.java
@@ -1,11 +1,15 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.DipsFragment;
+import polinema.ac.id.starterchapter05.fragments.HandstandFragment;
+import polinema.ac.id.starterchapter05.fragments.PushupsFragment;
 
 public class TugasActivity extends AppCompatActivity {
 
@@ -16,11 +20,23 @@ public class TugasActivity extends AppCompatActivity {
     }
 
     public void handlePushUp(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contentFragment, new PushupsFragment())
+                .addToBackStack(null)
+                .commit();
     }
 
     public void handleDips(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contentFragment, new DipsFragment())
+                .addToBackStack(null)
+                .commit();
     }
 
     public void handleHandstand(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contentFragment, new HandstandFragment())
+                .addToBackStack(null)
+                .commit();
     }
 }
